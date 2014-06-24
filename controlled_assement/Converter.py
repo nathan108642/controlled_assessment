29ad001cb7411409ffacbfc68850b489f5fb4939
nathan <nathanhayter@hotmail.com>
2014-06-24 14:25:26 +0100

    Create Converter.py

---
+++ controlled_assement/Converter.py
@@ -0,0 +1,26 @@
+allowables = ["pounds", "dollars", "euro", "yen"]
+rates = [1,1.68,1.23,171.61]
+def getChoice(dir):
+    answer = None
+    while answer not in range(len(allowables)):
+        print('Please type the currency code you wish to convert {0}'.format(dir))
+        for index, currency in enumerate(allowables):
+            print ('enter {0} for {1}'.format(index, currency))
+        answer = input("Please type what currency you wish to convert {0} ".format(dir))
+    return (int(answer))
+pounds = 'pounds'
+dollars = 'dollars'
+yen = 'yen'
+euro = 'euro'
+print("Welcome to the currency converter")
+
+var1 = getChoice ('from')
+
+var2 = getChoice ('to')
+
+var3 = float(input("Please type the amount of currency you wish to convert "))
+
+###Development 
+
+ammount = var3/rates[var1] *rates[var2]
+print(' your converted ammount is {0} {1}'.format(ammount,allowables[var2]))
